61123ea6f9128eb68c707cd99afca5ad9b468fae
Sharafaddin <>
2022-10-19 20:48:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#The total bill is 150$, split among 7 people, and the tip is 12%
+#The tip is (150 / 7) * 1.12 
+
+#What is the total bill? 150.00$
+#What percentage of the tip would like to give? 12$, 15$, or 20$?
+#Among how many people to divide this amount? 7
+#Per person need to give: 33.7$
+
+total_bill = input("Enter the total bill: ")
+percentage = input("What percentage would you like to give? ")
+
+  
