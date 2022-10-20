749f61af3b93aa703fbaaa8c43d4a42c8275812b
Sharafaddin Huseynov <114855172+Sharafaddin0@users.noreply.github.com>
2022-10-20 14:35:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-#The total bill is 150$, split among 7 people, and the tip is 12%
-#The tip is (150 / 7) * 1.12 
+# Greetings to my first project called as tip calculator
+print("Welcome to the tip calculator!")
 
-#What is the total bill? 150.00$
-#What percentage of the tip would like to give? 12$, 15$, or 20$?
-#Among how many people to divide this amount? 7
-#Per person need to give: 33.7$
+# First enter the total amount of bill
+bill = float(input("Enter the amount of bill: $"))
 
-total_bill = input("Enter the total bill: ")
-percentage = input("What percentage would you like to give? ")
+# Then enter what percentage of tip will be given
+tip = int(input("Enter the percentage of bill (10, 12, or 15) : %"))
 
-  
+# Then enter amoung how many people the total bill will be devided
+people = int(input("Enter the number of people: "))
+
+# Let's start the calculation of tip with bill
+tip_percentage = tip / 100
+bill_with_tip = tip_percentage * bill
+total_bill = bill_with_tip + bill
+
+# Then devide total bill among people
+per_person = total_bill / people
+
+# After that we need to round the final amount into 2 decimal units
+total_amount = round(per_person, 2)
+total_amount = "{:.2f}".format(total_amount)
+
+# Finally, by using F-strings print how much should be paid per person
+print(f'Each person should pay {total_amount} dollars')
